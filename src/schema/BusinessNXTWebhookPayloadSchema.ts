0c79bed924e5d1419215c36cd0b0e1b9b66feefd
Ole Melhus <ole@on-it.no>
2024-05-27 22:45:57 +0200

    add sample payload

--- src/schema/BusinessNXTWebhookPayloadSchema.ts
+++ src/schema/BusinessNXTWebhookPayloadSchema.ts
@@ -1,5 +1,20 @@
 import { z } from "zod";
 
+/* Sample payload
+{
+   "tableIdentifier":"Product",
+   "customerNo":0,
+   "companyNo":0,
+   "primaryKeys":[
+      {
+         "ProductNo":"1001"
+      }
+   ],
+   "event":"UPDATE",
+   "timestamp":"2024-05-27T18:12:55.7720784Z"
+}
+*/
+
 export const BusinessNXTWebhookPayloadSchema = z.object({
   tableIdentifier: z.string(),
   customerNo: z.number(),
